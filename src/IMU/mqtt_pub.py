089bf674298970aba90c893f4a97c11d31ea24f5
Grace Tang <gtang12@gmail.com>
2020-11-11 11:02:55 -0800

    add mqtt for imu motions

---
+++ src/IMU/mqtt_pub.py
@@ -0,0 +1,41 @@
+import time
+import random
+from paho.mqtt import client as mqtt_client
+broker = 'broker.emqx.io'
+port = 1883
+topic = "180d/team7"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(client, action):
+     while True:
+         time.sleep(1)
+         msg = f"action: {action}"
+         result = client.publish(topic, msg)
+         # result: [0, 1]
+         status = result[0]
+         if status == 0:
+             print(f"Send `{msg}` to topic `{topic}`")
+         else:
+             print(f"Failed to send message to topic {topic}")
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client, "test action")
+
+
+if __name__ == '__main__':
+    run()
