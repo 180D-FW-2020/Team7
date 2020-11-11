1e07357a3a986b294fca7268c82a183b24679948
HirofumiKK <hikoichihara@gmail.com>
2020-11-11 02:36:02 -0800

    Added basic command options like start, stop, etc for the game.

--- src/Speech/live_speech.py
+++ src/Speech/live_speech.py
@@ -7,3 +7,17 @@ with sr.Microphone() as source:
 	text=r.recognize_google(audio_data)
 	print("You said: ")
 	print(text)
+	# find if certain words exist within said phrase
+	if(text.find("begin") != -1 or text.find("start") != -1):
+		print("The game will start soon!")
+	elif(text.find("fight") != -1):
+		print("Fighting mode!")
+	elif(text.find("stop") != -1 or text.find("pause") != -1):
+		print("Game has stopped! Say 'Resume' or 'Restart' or 'Continue' to come back!")
+		with sr.Microphone() as source2:
+			audio_data2=r.record(source2, duration=5)
+			print("Recognizing...")
+			text2=r.recognize_google(audio_data2)
+			print("You said: " + text2)
+			if(text2.find("resume") != -1 or text2.find("restart") != -1 or text2.find("continue") != -1):
+				print("Welcome back!")
