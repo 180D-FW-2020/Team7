251cab108fd6b8871d54691d822455b5010b6358
kms-uzuki <kensuzuki17@gmail.com>
2020-11-12 08:08:12 +0000

    added some prelim data collection test

--- src/IMU/datacollection.py
+++ src/IMU/datacollection.py
@@ -6,7 +6,10 @@ import adafruit_lsm9ds1
 
 
 header = ["time","accX","accY","accZ","gyroX","gyroY","gyroZ","temp"]
-with open("{}.csv".format(time.strftime('%Y%m%d-%H%M%S'), 'w')) as csvfile:
+
+fname = "./data/{}.csv".format(time.strftime('%Y%m%d-%H%M%S'))
+
+with open(fname, 'w') as csvfile:
     i2c = busio.I2C(board.SCL, board.SDA)
     s = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
@@ -14,7 +17,7 @@ with open("{}.csv".format(time.strftime('%Y%m%d-%H%M%S'), 'w')) as csvfile:
     csvwriter.writerow(header)
     t = time.time()
     while 1:
-        buffer = [time.time - t]
+        buffer = [time.time() - t]
         buffer.extend(s.acceleration)
         buffer.extend(s.gyro)
         buffer.append(s.temperature)
