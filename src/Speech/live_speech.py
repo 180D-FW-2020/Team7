ab143858dff42757f1724b674161107e553c7732
HirofumiKK <hikoichihara@gmail.com>
2020-12-14 16:42:28 -0800

    embeded publisher in speech module

--- src/Speech/live_speech.py
+++ src/Speech/live_speech.py
@@ -1,6 +1,47 @@
+import time
+import random
+from paho.mqtt import client as mqtt_client
+broker = 'broker.emqx.io'
+port = 1883
+topic = "180d/team7"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(client, action):
+     while True:
+         time.sleep(1)
+         msg = f"Speech: {action}"
+         result = client.publish(topic, msg)
+         # result: [0, 1]
+         status = result[0]
+         if status == 0:
+             print(f"Send `{msg}` to topic `{topic}`")
+         else:
+             print(f"Failed to send message to topic {topic}")
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client, text)
+
+
+
 import speech_recognition as sr
 r=sr.Recognizer()
 print("Please Talk!")
+text = ""
 with sr.Microphone() as source:
 	audio_data=r.record(source, duration=5)
 	print("Recognizing...")
@@ -21,3 +62,8 @@ with sr.Microphone() as source:
 			print("You said: " + text2)
 			if(text2.find("resume") != -1 or text2.find("restart") != -1 or text2.find("continue") != -1):
 				print("Welcome back!")
+	if __name__ == '__main__':
+		run()
+
+
+
