7a41cc3f4ccba4915d23e05e82fd1f526ce28a2f
kms-uzuki <kensuzuki17@gmail.com>
2021-01-26 00:34:37 -0800

    data collection code for pi

--- src/IMU/datacollection.py
+++ src/IMU/datacollection.py
@@ -1,121 +1,107 @@
 import csv
 import time
-import board
-import busio
-import adafruit_lsm9ds1
-import RPi.GPIO as GPIO
+import sys
+import IMU
 
-from collections import deque
-
-DEBUG = 0
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+DEBUG = 1
 
 fname = "./data/{}.csv".format(time.strftime('%Y%m%d-%H%M%S'))
 
-with open(fname, 'w') as csvfile:
-    i2c = busio.I2C(board.SCL, board.SDA)
-    imu = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
-
+'''
     imu.accel_range = adafruit_lsm9ds1.ACCELRANGE_4G
     imu.gyro_scale = adafruit_lsm9ds1.GYROSCALE_2000DPS
 
     punchReg = False
     punchTime = time.time()
+'''
 
-    windowSize = 20
-    pthreshold = 14
-    gthreshold = -150
 
-    header = ["time","aX","aY","aZ","gX","gY","gZ","faX","faY","faZ","fgX","fgY","fgZ","temp","punchReg","punchCount",f"{windowSize}",f"{pthreshold}",f"{gthreshold}"]
+RAD_TO_DEG = 57.29578
+M_PI = 3.14159265358979323846
+_XL_MG_8G = 0.2440       
+_GYRO_DPS = 0.0700      
+XL_LPF_FACTOR = 0.4    # Low pass filter constant for accelerometer
+GYRO_LPF_FACTOR = 0.4
+#window = 40
+_SENSORS_GRAVITY_STANDARD = 9.80665
+
+oldXGyrRawValue = 0
+oldYGyrRawValue = 0
+oldZGyrRawValue = 0
+oldXAccRawValue = 0
+oldYAccRawValue = 0
+oldZAccRawValue = 0
+
+############ Helper functions to convert 16bit unsigned to readable data ############
+
+def _accel(raw):
+    return map(lambda x: x * _XL_MG_8G / 1000.0 * _SENSORS_GRAVITY_STANDARD, raw)
+
+def _gyro(raw):
+    return map(lambda x: x * _GYRO_DPS, raw)
+
+############     won't deal with magnetometer. fuck the magnetometer.    ############
+
+IMU.detectIMU()     #Detect if BerryIMU is connected.
+if(IMU.BerryIMUversion == 99):
+    print(" No BerryIMU found... exiting ")
+    sys.exit()
+IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
+
+
+
+with open(fname, 'w') as csvfile:
+
+
+    header = ["time","aX","aY","aZ","gX","gY","gZ","faX","faY","faZ","fgX","fgY","fgZ"]
 
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(header)
-    t = time.time()
+    a = time.perf_counter()
 
-    _accX = deque()
-    _accY = deque()
-    _accZ = deque()
-    _gyX = deque()
-    _gyY = deque()
-    _gyZ = deque()
-#    _magX = deque()
-#    _magY = deque()
-#    _magZ = deque()
+    while True:
 
-    while len(_accX) < windowSize:
-        ax,ay,az = imu.acceleration
-        gx,gy,gz = imu.gyro
-#        mx,my,mz = imu.magnetic
-        _accX.append(ax)
-        _accY.append(ay)
-        _accZ.append(az)
-        _gyX.append(gx)
-        _gyY.append(gy)
-        _gyZ.append(gz)
-#        _magX.append(mx)
-#        _magY.append(my)
-#        _magZ.append(mz)
+        #Read the accelerometer,gyroscope and magnetometer values
+        _ACCx = IMU.readACCx()
+        _ACCy = IMU.readACCy()
+        _ACCz = IMU.readACCz()
+        _GYRx = IMU.readGYRx()
+        _GYRy = IMU.readGYRy()
+        _GYRz = IMU.readGYRz()
 
-    punchCounter = 0
-    while 1:
-        ax,ay,az = imu.acceleration
-        gx,gy,gz = imu.gyro
-#        mx,my,mz = imu.magnetic
-        temp = imu.temperature
+        ###############################################
+        #### Apply low pass filter ####
+        ###############################################
+        GYRx =  _GYRx  * GYRO_LPF_FACTOR + oldXGyrRawValue*(1 - GYRO_LPF_FACTOR)
+        GYRy =  _GYRy  * GYRO_LPF_FACTOR + oldYGyrRawValue*(1 - GYRO_LPF_FACTOR)
+        GYRz =  _GYRz  * GYRO_LPF_FACTOR + oldZGyrRawValue*(1 - GYRO_LPF_FACTOR)
+        ACCx =  _ACCx  * XL_LPF_FACTOR + oldXAccRawValue*(1 - XL_LPF_FACTOR)
+        ACCy =  _ACCy  * XL_LPF_FACTOR + oldYAccRawValue*(1 - XL_LPF_FACTOR)
+        ACCz =  _ACCz  * XL_LPF_FACTOR + oldZAccRawValue*(1 - XL_LPF_FACTOR)
 
-        _accX.popleft()
-        _accY.popleft()
-        _accZ.popleft()
-        _gyX.popleft()
-        _gyY.popleft()
-        _gyZ.popleft()
-#        _magX.popleft()
-#        _magY.popleft()
-#        _magZ.popleft()
+        oldXGyrRawValue = GYRx
+        oldYGyrRawValue = GYRy
+        oldZGyrRawValue = GYRz
+        oldXAccRawValue = ACCx
+        oldYAccRawValue = ACCy
+        oldZAccRawValue = ACCz
 
-        _accX.append(ax)
-        _accY.append(ay)
-        _accZ.append(az)
-        _gyX.append(gx)
-        _gyY.append(gy)
-        _gyZ.append(gz)
-#        _magX.append(mx)
-#        _magY.append(my)
-#        _magZ.appendleft(mz)
+        ##################### END Ozzmaker code ########################
 
-        avaX = sum(_accX) / len(_accX)
-        avaY = sum(_accY) / len(_accY)
-        avaZ = sum(_accZ) / len(_accZ)
-        avgX = sum(_gyX) / len(_gyX)
-        avgY = sum(_gyY) / len(_gyY)
-        avgZ = sum(_gyZ) / len(_gyZ)
-#        avmX = sum(_magX) / len(_magX)
-#        avmY = sum(_magY) / len(_magY)
-#        avmZ = sum(_magZ) / len(_magZ)
+        buffer = [time.perf_counter() - a]
+        #add raw vals to data
+        buffer.extend(_accel((_ACCx,_ACCy,_ACCz)))
+        buffer.extend(_gyro((_GYRx,_GYRy,_GYRz)))
+        #add filtered vals to data
+        buffer.extend(_accel((ACCx,ACCy,ACCz)))
+        buffer.extend(_gyro((GYRx,GYRy,GYRz)))
 
-        if avaX > pthreshold and avgX < gthreshold and punchReg == False: 
-            punchReg = True
-            punchCounter += 1
-            print(f"Punch! {punchCounter}", end='\n')
-            GPIO.output(18,GPIO.HIGH)
-            punchTime = time.time()
-        if time.time() - punchTime >= 1:
-            punchReg = False
-            GPIO.output(18,GPIO.LOW)
+        # will compare LPF to moving average
+        # theoretically LPF has better cutoff & control
+        # but moving average is hella easy (just require queue, 
+        #       time complexity to add items is much better)
 
-        buffer = [time.time() - t]
-        buffer.extend(imu.acceleration)
-        buffer.extend(imu.gyro)
-#        buffer.extend(imu.magnetic)
-        buffer.extend((avaX, avaY, avaZ))
-        buffer.extend((avgX, avgY, avgZ))
-#        buffer.extend((avmX, avmY, avmZ))
-        buffer.append(temp)
-        buffer.append(punchReg)
-        buffer.append(punchCounter)
 
         if DEBUG:
-            print(buffer, end='	\r', flush=True)
+            print(buffer, end='                 	\r', flush=True)
         csvwriter.writerow(buffer)
