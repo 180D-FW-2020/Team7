8ba5d03a33ceaa32d32bcf87462590dc50c49cb7
Omar Radwan <omaryradwan@gmail.com>
2021-02-01 22:17:14 -0800

    adding nice client stuff

--- src/pose_client/vid_client.py
+++ src/pose_client/vid_client.py
@@ -16,13 +16,14 @@ import signal
 fps_time = 0
 debug = False
 use_video = False
-w =480
-h = 480
+w =432
+h = 368
 RESIZE_OUT_RATIO = 2.0
 host = '72.134.122.226'#THIS IS CONSTANT
 port = 5001 #SO IS THIS
 
-
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((host, port))
  #THIS IS CONSTANT
 
 
@@ -33,10 +34,6 @@ signal_catch = False;
 
 
 if __name__ == '__main__':
-
-
-
-
     parser = argparse.ArgumentParser(description='realtime broadcasting webcam')
     parser.add_argument('--input', default='camera')
     parser.add_argument('--video', type=str, default='')
@@ -62,16 +59,20 @@ if __name__ == '__main__':
         exit();
     frame_num = 0;
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))
+
+
 
 
     while True:
         frame_num += 1;
         print(frame_num)
-        if frame_num % 5 == 0:
+        if frame_num % 35 == 0:
+
             ok, frame = device.read()
-            frame = cv2.resize(frame,(480,480));
+            try:
+                frame = cv2.resize(frame,(480,480));
+            except:
+                break;
             #frame = cv2.resize(frame, (0,0), fx = 0.5, fy = 0.5)
             data = pickle.dumps(frame)
 
@@ -88,19 +89,19 @@ if __name__ == '__main__':
             current_time = now.strftime("%M:%S")
             print(current_time);
             print("Current frame:" + str(frame_num));
-
+            object = cv2();
             if(signal_catch == True):
                 exit(0);
             if debug:
-                cv2.putText(frame,
+                object.putText(frame,
                             "FPS: %f" % (1.0 / (time.time() - fps_time)),
                             (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                             (0, 255, 0), 2);
-
                 cv2.imshow('tf-pose-estimation result VIDEO/WEBCAM', frame)
                 if cv2.waitKey(1) == 27:
                     break
             fps_time = time.time()
-
+            #client_socket.close();
     device.release()
+    client_socket.close()
     cv2.destroyAllWindows()
