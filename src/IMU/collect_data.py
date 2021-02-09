b5e048e7537ca2b9bd56a964cab0a2a3e231e2f3
kms-uzuki <kensuzuki17@gmail.com>
2021-02-09 15:22:25 -0800

    remove filtering

--- src/IMU/collect_data.py
+++ src/IMU/collect_data.py
@@ -4,14 +4,14 @@ import pandas as pd
 import os
 import sys
 # import argparse
-from collections import deque
+# from collections import deque
 
 
 RAD_TO_DEG = 57.29578
 M_PI = 3.14159265358979323846
 _XL_MG_8G = 0.2440       
 _GYRO_DPS = 0.0700      
-windowSize = 10
+# windowSize = 10
 _SENSORS_GRAVITY_STANDARD = 9.80665
 
 
@@ -54,15 +54,15 @@ while True:
         sys.exit("Exited code.")
 
     data = []
-    _accX = deque(); _accY = deque(); _accZ = deque()
-    _gyrX = deque(); _gyrY = deque(); _gyrZ = deque()
+    # _accX = deque(); _accY = deque(); _accZ = deque()
+    # _gyrX = deque(); _gyrY = deque(); _gyrZ = deque()
 
-    while len(_accX) < windowSize: ## setup for filter
-        ax,ay,az = _accel((IMU.readACCx(),IMU.readACCy(),IMU.readACCz()))
-        gx,gy,gz = _gyro((IMU.readGYRx(),IMU.readGYRy(),IMU.readGYRz()))
+    # while len(_accX) < windowSize: ## setup for filter
+    #     ax,ay,az = _accel((IMU.readACCx(),IMU.readACCy(),IMU.readACCz()))
+    #     gx,gy,gz = _gyro((IMU.readGYRx(),IMU.readGYRy(),IMU.readGYRz()))
 
-        _accX.append(ax); _accY.append(ay); _accZ.append(az)
-        _gyrX.append(gx); _gyrY.append(gy); _gyrZ.append(gz)
+    #     _accX.append(ax); _accY.append(ay); _accZ.append(az)
+    #     _gyrX.append(gx); _gyrY.append(gy); _gyrZ.append(gz)
 
     countdown(3)
 
@@ -82,22 +82,23 @@ while True:
 
         # data processing #
         
-        ax,ay,az = _accel((_ACCx,_ACCy,_ACCz))
-        gx,gy,gz = _gyro((_GYRx,_GYRy,_GYRz))
+        # ax,ay,az = _accel((_ACCx,_ACCy,_ACCz))
+        # gx,gy,gz = _gyro((_GYRx,_GYRy,_GYRz))
 
-        _accX.popleft(); _accY.popleft(); _accZ.popleft()
-        _gyrX.popleft(); _gyrY.popleft(); _gyrZ.popleft()
+        # _accX.popleft(); _accY.popleft(); _accZ.popleft()
+        # _gyrX.popleft(); _gyrY.popleft(); _gyrZ.popleft()
 
-        _accX.append(ax); _accY.append(ay); _accZ.append(az)
-        _gyrX.append(gx); _gyrY.append(gy); _gyrZ.append(gz)
+        # _accX.append(ax); _accY.append(ay); _accZ.append(az)
+        # _gyrX.append(gx); _gyrY.append(gy); _gyrZ.append(gz)
 
-        avaX = sum(_accX)/len(_accX); avaY = sum(_accY)/len(_accY); avaZ = sum(_accZ)/len(_accZ)
-        avgX = sum(_gyrX)/len(_gyrX); avgY = sum(_gyrY)/len(_gyrY); avgZ = sum(_gyrZ)/len(_gyrZ)
+        # avaX = sum(_accX)/len(_accX); avaY = sum(_accY)/len(_accY); avaZ = sum(_accZ)/len(_accZ)
+        # avgX = sum(_gyrX)/len(_gyrX); avgY = sum(_gyrY)/len(_gyrY); avgZ = sum(_gyrZ)/len(_gyrZ)
 
         ###################
 
         row = [elapsed_ms, int(elapsed_ms - previous_elapsed_ms)] 
-        row.extend((avaX, avaY, avaZ)); row.extend((avgX, avgY, avgZ))
+        row.extend((_ACCx, _ACCy, _ACCz)); row.extend((_GYRx, _GYRY, _GYRz))
+        # row.extend((avaX, avaY, avaZ)); row.extend((avgX, avgY, avgZ))
 
         data.append(row)
         previous_elapsed_ms = elapsed_ms
